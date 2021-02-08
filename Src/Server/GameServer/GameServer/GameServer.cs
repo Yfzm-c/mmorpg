2b1e4f4fbed735b8de2f42633dfa720d196e54d5
dhr <2755184@qq.com>
2021-02-09 00:07:21 +0800

    1209

--- Src/Server/GameServer/GameServer/GameServer.cs
+++ Src/Server/GameServer/GameServer/GameServer.cs
@@ -24,8 +24,9 @@ namespace GameServer
         {
             network = new NetService();
             network.Init(8000);
-            HelloWorldService.Instance.Init();
-            //DBService.Instance.Init();
+            UserService.Instance.Init();
+            //HelloWorldService.Instance.Init();
+            DBService.Instance.Init();
             //var a = DBService.Instance.Entities.Characters.Where(s => s.TID == 1);
             //Console.WriteLine("{0}",a.FirstOrDefault<TCharacter>().Name);
             thread = new Thread(new ThreadStart(this.Update));
@@ -36,7 +37,7 @@ namespace GameServer
         public void Start()
         {
             network.Start();
-            HelloWorldService.Instance.Start();
+            //HelloWorldService.Instance.Start();
             running = true;
             thread.Start();
         }
