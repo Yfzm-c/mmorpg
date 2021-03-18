54b92c2ad63a93084250363a827f97967619d5ab
dhr <2755184@qq.com>
2021-03-18 21:22:47 +0800

    update

--- Src/Client/Assets/Scripts/UI/UILogin.cs
+++ Src/Client/Assets/Scripts/UI/UILogin.cs
@@ -45,7 +45,8 @@ public class UILogin : MonoBehaviour {
         if (result == Result.Success)
         {
             //登录成功，进入角色选择
-            MessageBox.Show("登录成功，准备选择角色" + message, "提示", MessageBoxType.Information);
+            //MessageBox.Show("登录成功，准备选择角色" + message, "提示", MessageBoxType.Information);
+            SceneManager.Instance.LoadScene("CharSelect");
         }
         else
             MessageBox.Show(message, "错误", MessageBoxType.Error);
