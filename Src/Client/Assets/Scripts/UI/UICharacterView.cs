e2a998cb5c03121f64cf2e136d3d829c56519f12
dhr <2755184@qq.com>
2021-03-29 14:52:00 +0800

    完成第8课，角色创建与选择（1）

---
+++ Src/Client/Assets/Scripts/UI/UICharacterView.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UICharacterView : MonoBehaviour {
+
+    public GameObject[] characters;
+
+    private int currectCharacter = 0;
+
+    public int CurrectCharacter
+    {
+        get
+        {
+            return currectCharacter;
+        }
+        set
+        {
+            currectCharacter = value;
+            this.UpdateCharacter();
+        }
+    }
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void UpdateCharacter()
+    {
+        for(int i = 0; i < 3; i++)
+        {
+            characters[i].SetActive(i == this.currectCharacter);
+        }
+    }
+}
